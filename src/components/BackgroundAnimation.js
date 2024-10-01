1ad0e0d52d541006e5e2edeefa25b3c691bc34a3
MHC32 <hantzmichaelchery6@gmail.com>
2024-10-01 17:33:09 -0400

    background animation setup is okay

--- src/components/BackgroundAnimation.js
+++ src/components/BackgroundAnimation.js
@@ -2,18 +2,76 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
-const data = [
+const slide1 = [
   {
     id: 1,
     image: require('../../assets/images/cover/fifa23.png')
   },
 
   {
-    id: 1,
-    image: require('../../assets/images/cover/fifa23.png')
-  }
+    id: 2,
+    image: require('../../assets/images/cover/need-for-speed.png')
+  },
+
+  {
+    id: 3,
+    image: require('../../assets/images/cover/gotham-knights.png')
+  },
+
+  {
+    id: 4,
+    image: require('../../assets/images/cover/red-dead-redemption.png')
+  },
 ]
 
+const slide2 = [
+  {
+    id: 1,
+    image: require('../../assets/images/cover/dead-by-day-light.png')
+  },
+
+  {
+    id: 2,
+    image: require('../../assets/images/cover/the-witcher.png')
+  },
+
+  {
+    id: 3,
+    image: require('../../assets/images/cover/wonderlands.png')
+  },
+
+  {
+    id: 4,
+    image: require('../../assets/images/cover/star-wars-lego.png')
+  },
+
+  {
+    id: 5,
+    image: require('../../assets/images/cover/cyberpunk.png')
+  },
+]
+
+const slide3 = [
+  {
+    id: 1,
+    image: require('../../assets/images/cover/bordelands3.png')
+  },
+
+  {
+    id: 2,
+    image: require('../../assets/images/cover/ghost-recorn.png')
+  },
+
+  {
+    id: 3,
+    image: require('../../assets/images/cover/riders-republic.png')
+  },
+
+  {
+    id: 4,
+    image: require('../../assets/images/cover/midnight-suns.png')
+  },
+]
 const BackgroundAnimation = () => {
   return (
     <View style={styles.container}>
